2cdd9939e8468ff119a9cf52c17a1529e180686d
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-14 23:49:01 +0200

    Scaffold job to update github info

---
+++ src/main/kotlin/com/codefarm/planter/jobs/GithubInfoUpdateJob.kt
@@ -0,0 +1,7 @@
+package com.codefarm.planter.jobs
+
+class GithubInfoUpdateJob {
+    // implement job that runs daily and calls GitHub to update:
+    // - user information
+    // - repository information
+}
