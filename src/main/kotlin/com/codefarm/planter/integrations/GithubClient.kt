24cf638b9b889e7a5bb227c1a777ae764011b2ee
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-15 00:28:35 +0200

    Make github repository id random

--- src/main/kotlin/com/codefarm/planter/integrations/GithubClient.kt
+++ src/main/kotlin/com/codefarm/planter/integrations/GithubClient.kt
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Service
 @Service
 class GithubClient {
 
-    fun createRepository(githubUserId: Long, repositoryName: String) = 1L // Returns a mock repository ID
+    fun createRepository(githubUserId: Long, repositoryName: String) = randomLong() // Returns a mock repository ID
+
+    fun randomLong() = (0..9999L).random()
 }
