1818034ab7f9cabe7df531675456fad8c78e016f
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-07-29 22:41:59 +0200

    Fix user service

--- src/main/kotlin/studio/vitr/planter/service/ProjectService.kt
+++ src/main/kotlin/studio/vitr/planter/service/ProjectService.kt
@@ -23,8 +23,6 @@ class ProjectService(
     fun create(user: User, request: ProjectRequest) =  githubClient
         .createRepository(user.githubUserId, request.name)
         .let { adapter.toProject(request, user, it) }
-        .also { user.projects.add(it) }
-        .also { it.user = user }
         .let { projectRepository.save(it) }
 
     fun delete(id: UUID) = projectRepository.deleteById(id)
